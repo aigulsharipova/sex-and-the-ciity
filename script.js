8ff571b97881b1d18ed50a3a25ad4184e1c80c10
Aigul <sharipova.aig@gmail.com>
2022-10-04 19:12:00 -0400

    const button

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const image = document.querySelector('#myImage');
 const text = document.querySelector('h2');
+
+const btnOne = document.querySelector('#one');
+const btnTwo = document.querySelector('#two');

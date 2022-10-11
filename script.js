441277249df944fbc61ef8eafc6e37a31cecb8b9
Aigul <sharipova.aig@gmail.com>
2022-10-10 20:48:55 -0400

    function 2nd

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const btnTwo = document.querySelector('#two');
 
 btnOne.addEventListener('click', mrBig);
 function mrBig() {
-  image.setAttribute("src", "./style/photo2.jpg");
-  text.innerHTML = "Will they get married?"
+image.setAttribute("src", "./style/photo2.jpg");
+text.innerHTML = "Will they get married?"
+}
+
+btnTwo.addEventListener('click', aidon);
+function aidon() {
+image.setAttribute("src", "./style/photo3.webp");
+text.innerHTML = "Will they get married?"
 }
